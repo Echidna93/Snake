97decda96d68c68aa3db59efd82f68b2fcd9fcca
Alexander Jack <jackx022@umn.edu>
2020-05-13 21:28:58 -0500

    initial commit

---
+++ Game.java
@@ -0,0 +1,78 @@
+import javax.swing.JPanel;
+import java.awt.event.KeyListener;
+import java.util.Arrays;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Game extends JPanel implements ActionListener, KeyListener {
+
+    /**
+     *
+     */
+
+    private static final long serialVersionUID = 1L;
+    protected String direction;
+    public int[][] bodySegment = new int[][]{{100, 100, 10, 10},{110, 100, 10, 10}};
+    public Game(){
+        setFocusable(true);
+        addKeyListener(this);    
+        //Image h = this.head.getScaledInstance(10, 10, 1);
+        setSize(400,400);
+        setBackground(Color.WHITE);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        //g.clearRect(this.bodysegment[], y, width, height);
+        // TODO Auto-generated method stub
+        super.paintComponent(g);
+        for(int i = 0; i < bodySegment.length; i++){
+        g.fillOval(bodySegment[i][0], bodySegment[i][1], bodySegment[i][2], bodySegment[i][3]);
+        }
+        g.setColor(Color.BLUE);
+        //super.paintComponent(g);
+        //g.dispose();
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+ 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //System.out.println("inside keyPressed event ");
+     if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+        int length = this.bodySegment.length;
+        // take tail attach it to head
+        System.out.println(Arrays.toString(bodySegment[0]));
+        // step the array
+        bodySegment[0][0] = bodySegment[length-1][0] + bodySegment[length-1][2];
+        // swap element 1 and 2
+        int[] tmpArray = bodySegment[0];
+        bodySegment[0] = bodySegment[length-1];
+        bodySegment[length-1] = tmpArray;
+        System.out.println("head " + Arrays.toString(bodySegment[length-1]));
+        System.out.println("tail " + Arrays.toString(bodySegment[0]));
+        repaint();
+     }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+     
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        Game game = new Game();
+
+        frame.setSize(400, 400);
+        frame.setTitle("SnEk");
+        frame.setVisible(true);
+        //Game game = new Game();
+        frame.setContentPane(game);
+        game.setFocusable(true);
+    }
+}
