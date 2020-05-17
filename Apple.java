c24d398260fc8af0456adf82be58ece7c23544d3
Alexander Jack <jackx022@umn.edu>
2020-05-17 14:03:14 -0500

    add apple. class to hold basic information about apple objects add isCollision method to determine if apple and snake are hitting eachother

---
+++ Apple.java
@@ -0,0 +1,26 @@
+public class Apple {
+    
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+
+    public Apple(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+    public int getWidth(){
+        return this.width;
+    }
+    public int getHeight(){
+        return this.height;
+    }
+}
--- Game.java
+++ Game.java
@@ -6,15 +6,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Game extends JPanel implements ActionListener, KeyListener {
     private static final long serialVersionUID = 1L;
-    public int BOARD_WIDTH;
-    public int BOARD_HEIGHT;
+    public int BOARD_WIDTH = 400;
+    public int BOARD_HEIGHT = 400;
     // field to keep track of snake head hitting the border of the game board
     public String isOnWhichBorder = "";
     Snake snake = new Snake();
-
+    int randX = (int)(Math.random() * ((BOARD_WIDTH) + 1));
+    int randY = (int)(Math.random() * ((BOARD_HEIGHT) + 1));
+    Apple apple = new Apple(randX, randY, 20, 20);
     public int[] RIGHT = new int[]{snake.getOvalHeight(), 0};
     public int[] LEFT = new int[]{-snake.getOvalHeight(), 0};
     public int[] UP = new int[]{0,-snake.getOvalHeight()};
@@ -27,41 +30,53 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 
         setFocusable(true);
         // TODO: check where keyListener actually needs to go
-        addKeyListener(this);
-        this.BOARD_WIDTH = 400;
-        this.BOARD_HEIGHT = 400;    
+        addKeyListener(this);    
         setSize(BOARD_WIDTH, BOARD_HEIGHT);
         setBackground(Color.WHITE);
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(final Graphics g) {
         //g.clearRect(this.bodysegment[], y, width, height);
         // TODO Auto-generated method stub
         super.paintComponent(g);
+        g.setColor(Color.GREEN);
         for(int i = 0; i < snake.getLength(); i++){
             g.fillOval(snake.getBodyXComponent(i), snake.getBodyYComponent(i), snake.getOvalHeight(), snake.getOvalWidth());
             //System.out.println("this is body segment : " + snake.getBodySegment(i));
         }
-        g.setColor(Color.BLUE);
+        g.setColor(Color.RED);
+        g.fillOval(apple.getX(), apple.getY(), apple.getWidth(), apple.getHeight());
         //super.paintComponent(g);
         //g.dispose();
     }
     /*
      * @param isOnWhichBorder the isOnWhichBorder to set
      */
-    public void setIsOnWhichBorder(String isOnWhichBorder) {
+    public void setIsOnWhichBorder(final String isOnWhichBorder) {
         this.isOnWhichBorder = isOnWhichBorder;
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(final KeyEvent e) {
         // TODO Auto-generated method stub
 
     }
+
+    // method to check if snake and apple are occupying the same point in the game board
+    public boolean isCollision(Apple apple, Snake snake){
+        //System.out.println("Snake position : " + Arrays.toString(snake.getHead()));
+        //System.out.println("Apple position : " + "(" + apple.getX() + apple.getY() + ")");
+        //(snake.getHead()[0] <= (apple.getX() + apple.getWidth()))) && (snake.getHead()[1] >= Math.abs(apple.getX() - apple.getWidth()) && (snake.getHead()[1] <= (apple.getY() - apple.getHeight())))
+        boolean isCollision = false;
+        if(((snake.getHead()[0] >= Math.abs(apple.getX() - apple.getWidth())) && (snake.getHead()[0] <= (apple.getX() + apple.getWidth()))) && ((snake.getHead()[1] >= Math.abs(apple.getY() - apple.getHeight())) && (snake.getHead()[1] <= (apple.getY() + apple.getHeight())))){
+            isCollision = true;
+        }
+        return isCollision;
+    }
  
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(final KeyEvent e) {
         //System.out.println("inside keyPressed event ");
      if(snake.getHead()[0] == 0 && !(snake.getBodyXComponent(1) == 400)){
         isOnWhichBorder = "LEFT";
@@ -89,21 +104,22 @@ public class Game extends JPanel implements ActionListener, KeyListener {
             direction = DOWN;
             break;
         }
-        System.out.println(isOnWhichBorder);
         snake.slither(isOnWhichBorder, direction);
+        if(isCollision(apple, snake)){
+            System.out.println("snake and apple have collided");
+        }
         this.setIsOnWhichBorder("");
-        System.out.println(isOnWhichBorder);
         repaint();
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(final KeyEvent e) {
         // TODO Auto-generated method stub
 
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(final ActionEvent e) {
      
     }
     
