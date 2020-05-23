3387c6548b31352ef6cef7fd6b4632abc0947fb0
Alexander Jack <jackx022@umn.edu>
2020-05-23 17:58:41 -0500

    add startGame field, add setStartGame, add actionPerformed

--- InitMenu.java
+++ InitMenu.java
@@ -1,6 +1,7 @@
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
 import java.util.Arrays;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,18 +10,33 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-public class InitMenu extends JPanel {
+public class InitMenu extends JPanel implements ActionListener {
     /**
      *
      */
     private static final long serialVersionUID = 1L;
+
+    boolean startGame;
+
     JButton startButton = new JButton("New Game");
-    public InitMenu(){
-        add(startButton);           
+
+    public InitMenu() {
+        add(startButton);
+        startButton.addActionListener(this);
+        startGame = false;
     }
     @Override
     protected void paintComponent(Graphics g){
         
     }
-    
+    public void setStartGame(boolean startgame){
+        this.startGame = startgame;
+    }
+    public boolean getStartGame(){
+        return this.startGame;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        setStartGame(true);   
+    }
 }
--- Main.java
+++ Main.java
@@ -1,20 +1,29 @@
+import java.awt.event.MouseListener;
+
 import javax.swing.JFrame;
 import sun.security.x509.IssuerAlternativeNameExtension;
+
+// TODO: read up on layouts, add layout to the game
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame();
-        Game game = new Game();
-        frame.setSize(400, 400);
-        frame.setTitle("SnEk");
+        InitMenu initMenu = new InitMenu();
+        do{
+        frame.setContentPane(initMenu);
         frame.setVisible(true);
-        //Game game = new Game();
-        frame.setContentPane(game);
-        game.setFocusable(true);
-        frame.addKeyListener(game);
-        while(game.isAlive){
-            game.play();
-            Thread.sleep(100);
+        }while(!initMenu.getStartGame());
+            Game game = new Game();
+            frame.setSize(400, 400);
+            frame.setTitle("SnEk");
+            frame.setVisible(true);
+            //Game game = new Game();
+            frame.setContentPane(game);
+            game.setFocusable(true);
+            frame.addKeyListener(game);
+                while(game.isAlive){
+                game.play();
+                Thread.sleep(100);
+            }
         }
     }
-}
